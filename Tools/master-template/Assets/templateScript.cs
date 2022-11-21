6b5a40d4b6836e83daf471da8623e82d4bf00488
DJHero2903 <67331181+DJHero2903@users.noreply.github.com>
2022-11-21 19:57:37 +0000

    Adding mod creation tools

---
+++ Tools/master-template/Assets/templateScript.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+
+public class templateScript : MonoBehaviour
+{
+	public KMAudio Audio;
+	public KMBombModule BombModule;
+	public KMBombInfo Info;
+	
+	//public KMSelectable Button;
+
+
+	//Logging
+	static int moduleIdCounter = 1;
+	int moduleId;
+
+	bool Solved = false;
+
+	void  Awake()
+	{
+		moduleId = moduleIdCounter++;
+		//Button.OnInteract += delegate () { Button_Pressed(); return false; };
+
+	}
+
+ void Activate()
+    {
+        
+        
+    }
+}
